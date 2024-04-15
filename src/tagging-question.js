8a35e6963b85b3c2b01c40769450b41902d841aa
williamguthier <willg8766@gmail.com>
2024-04-14 22:25:14 -0400

    stuff

--- src/tagging-question.js
+++ src/tagging-question.js
@@ -13,13 +13,42 @@ export class TaggingQuestion extends LitElement {
 
   static get styles() {
     return css`
-      :host {
-
-      }
+      
     `;
   }
 
   render() {
+    return html`
+        <div class="tagging-question">
+            <div class="qeustion-wrapper">
+                <p class="question">Which of the following big ideas would YOU associate with this artistic work?</p>
+                <div class="image">
+                    <img src="https://art-tec.co.uk/wp-content/uploads/2019/01/Canvas-Vs.-Artistic-Work-Paper.jpg" alt="Artistic work">
+                </div>
+                </div>
+            </div>
+        <div class="tag-wrapper">
+            <div class="tag" draggable="true" ></div>
+        </div>
+        <div class="answer-wrapper">
+
+        </div>
+        <div class="feedback-wrapper">
+
+        </div>
+        <div class="buttons">
+            <button class="check-answer" @click="${this.checkAnswer}">Check Answer</button>
+            <button class="reset" @click="${this.reset}">Reset</button>
+        </div>
+    </div>
+    `;
+  }
+
+  checkAnswer() {
+
+  }
+
+  reset() {
 
   }
 
